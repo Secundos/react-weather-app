cb151d2dc399ec1732e5ea93e5a2507253fde92d
Secundos <77840586+Secundos@users.noreply.github.com>
2021-04-05 10:03:20 +0200

    Added GitHub repo and IN link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather app
+     <h1>Weather app</h1>
+     <p>This app was coded by{" "}< a href="https://www.linkedin.com/in/emilia-kuci%C4%99ba-a497761ba/" target="_blank">Emilia Kucięba</a>  and it's open-soursed {" "} <a href ="https://github.com/Secundos/react-weather-app" target="_blank">on GitHub </a>
+      </p> 
+    
     </div>
   );
 }
