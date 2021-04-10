da962aaeceb06d03d90da287d413b7f0502ae1ce
Secundos <77840586+Secundos@users.noreply.github.com>
2021-04-10 15:00:23 +0200

    Add basic css

--- src/Weather.js
+++ src/Weather.js
@@ -3,13 +3,11 @@ import "./Weather.css";
 
 export default function Weather(){
     return (<div className="Weather">
-        
-           
         <form><div className="row">
             <div className="col-9">
-            <input type="search" placeholder="Enter a city..." className="form-control"/></div>
-           <div className="col-3"> <input type="submit" value="Submit" className="btn btn-primary"/>
-            <input type="submit" value="Back to current" className="btn btn-success"/></div>
+            <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on"/></div>
+           <div className="col-3"> <input type="submit" value="Submit" className="btn btn-primary w-100"/>
+            <input type="submit" value="Back to current" className="btn btn-success w-100"/></div>
        </div> </form>
        
         <h1>Warsaw</h1>
@@ -17,10 +15,14 @@ export default function Weather(){
             <li> Monday 10:30</li>
             <li> Sunny</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
             <div className="col-6">
-                <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny" />
-                10°C|°F
+                <div className="clearfix">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny" className="float-left" />
+              <div className="float-left">
+               <span className="temp">10</span> 
+               <span className="units"> C|°F</span>°</div>
+            </div>
             </div>
             <div className="col-6">
                 <ul>
